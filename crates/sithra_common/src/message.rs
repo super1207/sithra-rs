579572070f8c34f5cd7dd021b2087f0e7f461e0d
BERADQ <adqber123@outlook.com>
2025-04-26 14:34:24 +0800

    refactor: fix message type error

--- crates/sithra_common/src/message.rs
+++ crates/sithra_common/src/message.rs
@@ -9,36 +9,31 @@ pub type SVec<T> = SmallVec<[T; 3]>;
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct SegmentRaw {
     pub r#type: String,
-    pub message_id: String,
     pub kv: Map<String, String, 3>,
 }
 
 impl SegmentRaw {
-    pub fn new(r#type: String, message_id: String, kv: KV) -> Self {
-        Self {
-            r#type,
-            message_id,
-            kv,
-        }
+    pub fn new(r#type: String, kv: KV) -> Self {
+        Self { r#type, kv }
     }
     // 基础消息段类型构造器
     /// 文本
-    pub fn text(message_id: String, text: String) -> Self {
+    pub fn text(text: String) -> Self {
         let mut kv = KV::new();
         kv.insert("content".to_string(), text);
-        Self::new("text".to_string(), message_id, kv)
+        Self::new("text".to_string(), kv)
     }
     /// 图片
-    pub fn image(message_id: String, url: String) -> Self {
+    pub fn image(url: String) -> Self {
         let mut kv = KV::new();
         kv.insert("url".to_string(), url);
-        Self::new("image".to_string(), message_id, kv)
+        Self::new("image".to_string(), kv)
     }
     /// 提及用户
-    pub fn at(message_id: String, user_id: String) -> Self {
+    pub fn at(user_id: String) -> Self {
         let mut kv = KV::new();
         kv.insert("user_id".to_string(), user_id);
-        Self::new("at".to_string(), message_id, kv)
+        Self::new("at".to_string(), kv)
     }
 }
 /// 消息段类型
@@ -62,31 +57,34 @@ pub trait MessageSerializer {
 /// 消息类型
 pub trait Message
 where
-    Self: IntoIterator<Item = Self::Segment> + FromIterator<Self::Segment> + Clone,
+    Self: IntoIterator<Item = Self::Segment> + Clone,
 {
     type Segment: Segment;
-    fn from_raw_iter(raw: impl IntoIterator<Item = SegmentRaw>) -> Self {
+    fn segments(
+        raw: impl IntoIterator<Item = SegmentRaw>,
+    ) -> impl Iterator<Item = Self::Segment> {
         raw.into_iter()
             .filter_map(<Self::Segment as Segment>::Deserializer::deserialize)
-            .collect()
     }
-    fn from_raw(raw: MessageRaw) -> Self {
-        Self::from_raw_iter(raw.segments)
-    }
+    fn from_raw(raw: MessageRaw) -> Self;
     fn into_raw(self) -> MessageRaw {
-        self.into_iter()
+        let id = self.id();
+        let segments = self
+            .into_iter()
             .filter_map(<Self::Segment as Segment>::Serializer::serialize)
-            .collect::<SVec<_>>()
-            .into()
+            .collect::<SVec<_>>();
+        MessageRaw::new(segments, id)
     }
+    fn id(&self) -> Option<String>;
 }
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct MessageRaw {
     pub segments: SVec<SegmentRaw>,
+    pub id: Option<String>,
 }
 impl MessageRaw {
-    pub fn new(segments: SVec<SegmentRaw>) -> Self {
-        Self { segments }
+    pub fn new(segments: SVec<SegmentRaw>, id: Option<String>) -> Self {
+        Self { segments, id }
     }
 }
 impl From<MessageRaw> for SVec<SegmentRaw> {
@@ -94,26 +92,18 @@ impl From<MessageRaw> for SVec<SegmentRaw> {
         value.segments
     }
 }
-impl From<SVec<SegmentRaw>> for MessageRaw {
-    fn from(value: SVec<SegmentRaw>) -> Self {
-        Self::new(value)
-    }
-}
 impl<T: Message> From<T> for MessageRaw {
     fn from(value: T) -> Self {
-        Self::new(value.into_raw().into())
+        let id = value.id();
+        let segments = value.into_raw().into();
+        Self::new(segments, id)
     }
 }
 
 pub mod common {
     use super::*;
     #[derive(Debug, Clone)]
-    pub struct CommonSegment {
-        pub id: String,
-        pub kind: CommonSegmentKind,
-    }
-    #[derive(Debug, Clone)]
-    pub enum CommonSegmentKind {
+    pub enum CommonSegment {
         /// 文本
         Text(String),
         /// 图片
@@ -121,12 +111,12 @@ pub mod common {
         /// 提及用户
         At(String),
     }
-    impl From<&mut SegmentRaw> for Option<CommonSegmentKind> {
+    impl From<&mut SegmentRaw> for Option<CommonSegment> {
         fn from(value: &mut SegmentRaw) -> Self {
             match value.r#type.as_str() {
-                "text" => Some(CommonSegmentKind::Text(value.kv.remove("content")?)),
-                "image" => Some(CommonSegmentKind::Image(value.kv.remove("url")?)),
-                "at" => Some(CommonSegmentKind::At(value.kv.remove("user_id")?)),
+                "text" => Some(CommonSegment::Text(value.kv.remove("content")?)),
+                "image" => Some(CommonSegment::Image(value.kv.remove("url")?)),
+                "at" => Some(CommonSegment::At(value.kv.remove("user_id")?)),
                 _ => None,
             }
         }
@@ -138,27 +128,25 @@ pub mod common {
     /// 一般消息类型。
     #[derive(Debug, Clone)]
     pub struct CommonMessage {
+        pub id: Option<String>,
         inner: SVec<CommonSegment>,
     }
     pub struct CommonMessageProcessor;
     impl MessageSerializer for CommonMessageProcessor {
         type Input = CommonSegment;
         fn serialize(message: Self::Input) -> Option<SegmentRaw> {
-            match message.kind {
-                CommonSegmentKind::Text(text) => Some(SegmentRaw::text(message.id, text)),
-                CommonSegmentKind::Image(url) => Some(SegmentRaw::image(message.id, url)),
-                CommonSegmentKind::At(user_id) => Some(SegmentRaw::at(message.id, user_id)),
+            match message {
+                CommonSegment::Text(text) => Some(SegmentRaw::text(text)),
+                CommonSegment::Image(url) => Some(SegmentRaw::image(url)),
+                CommonSegment::At(user_id) => Some(SegmentRaw::at(user_id)),
             }
         }
     }
     impl MessageDeserializer for CommonMessageProcessor {
         type Output = CommonSegment;
         fn deserialize(mut segment: SegmentRaw) -> Option<Self::Output> {
-            let kind = Option::<CommonSegmentKind>::from(&mut segment)?;
-            Some(CommonSegment {
-                id: segment.message_id,
-                kind,
-            })
+            let kind = Option::<CommonSegment>::from(&mut segment)?;
+            Some(kind)
         }
     }
     impl IntoIterator for CommonMessage {
@@ -168,14 +156,17 @@ pub mod common {
             self.inner.into_iter()
         }
     }
-    impl FromIterator<CommonSegment> for CommonMessage {
-        fn from_iter<T: IntoIterator<Item = CommonSegment>>(iter: T) -> Self {
-            Self {
-                inner: iter.into_iter().collect(),
-            }
-        }
-    }
     impl Message for CommonMessage {
         type Segment = CommonSegment;
+        fn id(&self) -> Option<String> {
+            self.id.clone()
+        }
+        fn from_raw(raw: MessageRaw) -> Self {
+            let segments = Self::segments(raw.segments).collect();
+            Self {
+                id: raw.id,
+                inner: segments,
+            }
+        }
     }
 }
